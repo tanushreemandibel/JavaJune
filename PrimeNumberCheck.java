b485e7e90d4c97d86afbe77461edc592979e3a8e
tanushreemandibel <86183713+tanushreemandibel@users.noreply.github.com>
2021-06-24 21:11:04 +0530

    primenumbercheck
    task8

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,34 @@
+package FirstCode;
+
+
+
+	import java.util.Scanner;
+
+	public class PrimeNumberCheck {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			  
+				  int i,m=0,flag=0;   
+				  System.out.println("Enter a number: ");
+				  Scanner sc = new Scanner(System.in); 
+				  int n = sc.nextInt();
+				  m=n/2;      
+				  if(n==0||n==1){  
+				   System.out.println(n+" is not prime number");      
+				  }else{  
+				   for(i=2;i<=m;i++){      
+				    if(n%i==0){      
+				     System.out.println(n+" is not prime number");      
+				     flag=1;      
+				     break;      
+				    }      
+				   }      
+				   if(flag==0)  { System.out.println(n+" is prime number"); }  
+				  }
+		}
+				   
+				
+		
+
+}
